d34926480e425c01cee0fe86e388799d8d9d34cf
irvansyachrudin <irvanbrow97@gmail.com>
2020-05-13 14:26:17 +0700

    #2 Templating & Authentication

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 require('./bootstrap');
+import Vue from 'vue'
+import router from './router.js'
+import store from './store.js'
+import App from './App.vue'
+
+new Vue({
+    el: '#dw',
+    router,
+    store,
+    components: {
+        App
+    }
+})
